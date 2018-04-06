31519fdf1145b96bb7002ab4c6e524476e99ef8e
Szymon Mendla <szymon.mendla@sap.com>
2018-04-06 10:23:23 +0200

    Create maven project

---
+++ src/main/java/com/goquizit/GoquizitApplication.java
@@ -0,0 +1,12 @@
+package com.goquizit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoquizitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoquizitApplication.class, args);
+	}
+}
---
+++ src/test/java/com/goquizit/GoquizitApplicationTests.java
@@ -0,0 +1,16 @@
+package com.goquizit;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GoquizitApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
