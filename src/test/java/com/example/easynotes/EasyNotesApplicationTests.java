65ffb33b5d77f6b7fe5f8d946402761cbc58cdbe
Szymon Mendla (szymmen835) <szymmen835@student.polsl.pl>
2018-04-14 12:49:44 +0200

    Projekt wersja z dzialajaca baza danych.

--- src/test/java/com/example/easynotes/EasyNotesApplicationTests.java
+++ src/test/java/com/example/easynotes/EasyNotesApplicationTests.java
@@ -1,6 +1,5 @@
-package com.goquizit.beta;
+package com.example.easynotes;
 
-import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -8,8 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class BetaApplicationTests {
-
+public class EasyNotesApplicationTests {
 
 	@Test
 	public void contextLoads() {
