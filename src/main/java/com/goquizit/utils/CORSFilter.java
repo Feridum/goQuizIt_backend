50b49f5a013766ba7d2c6fc11fbdd17eb2f4af97
Szymon Mendla (szymmen835) <szymmen835@student.polsl.pl>
2018-05-04 21:42:02 +0200

    Upgrade CORS

--- src/main/java/com/goquizit/utils/CORSFilter.java
+++ src/main/java/com/goquizit/utils/CORSFilter.java
@@ -37,6 +37,15 @@ public class CORSFilter extends OncePerRequestFilter {
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
         response.addHeader("Access-Control-Allow-Origin", "*");
 
+        if (request.getHeader("Access-Control-Request-Method") != null && "OPTIONS".equals(request.getMethod())) {
+            LOG.trace("Sending Header....");
+            // CORS "pre-flight" request
+            response.addHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+			response.addHeader("Access-Control-Allow-Headers", "Authorization");
+            response.addHeader("Access-Control-Allow-Headers", "Content-Type");
+            response.addHeader("Access-Control-Max-Age", "86400");
+        }
+
         filterChain.doFilter(request, response);
     }
 
