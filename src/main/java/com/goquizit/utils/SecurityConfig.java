ba0883f9f490c4aa3c6687bd1543f167cec1619b
Szymon Mendla (szymmen835) <szymmen835@student.polsl.pl>
2018-05-21 17:51:04 +0200

    Get quiz by token without authorization

--- src/main/java/com/goquizit/utils/SecurityConfig.java
+++ src/main/java/com/goquizit/utils/SecurityConfig.java
@@ -5,20 +5,19 @@ import com.goquizit.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-
-import javax.servlet.http.HttpServletResponse;
-
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import javax.servlet.http.HttpServletResponse;
 
 @Configuration
 @EnableWebSecurity
@@ -56,7 +55,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                         "/api/forgot-password**",
                         "/api/reset-password**",
                         "/swagger-ui.html",
-                        "/api/players/**").permitAll()
+                        "/api/players/**",
+                        "/api/quiz/token/**").permitAll()
                 .anyRequest().authenticated();
 
     }
